d58c8e55302a7fa0a3059b65bd2d1c82dcbf6af8
Eduardo Junior <juniorcose@gmail.com>
2022-09-19 21:38:08 -0300

    added html in site

--- receitas/views.py
+++ receitas/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>receitas</h1>')
+    return render(request, 'index.html')
+
+
+def receita(request):
+    return render(request, 'receita.html')
